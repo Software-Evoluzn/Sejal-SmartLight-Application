7255772e499e8fbbde11cee0651fd1bd48018d6f
KambleSejalSudhir <kamblesejal277@gmail.com>
2024-11-08 12:49:08 +0530

    Schedule table is created and insert function done

--- app/src/main/java/com/example/etcdynamiclight/DBHelpher.kt
+++ app/src/main/java/com/example/etcdynamiclight/DBHelpher.kt
@@ -12,44 +12,71 @@ class DBHelpher(context: Context,factory:SQLiteDatabase.CursorFactory?):
         companion object{
             private val DATABASE_NAME="Lights"
             private val DATABASE_VERSION=1
+
+
             val tableName="devices_query"
             val srNo_col="Sr_No"
             val device_id_col="Device_id"
+
+            val tableSchedule="scheduling"
+            val scheduling_Sr_No="Sch_Id"
+            val scheduling_everyDay="Sch_everyDay"
+            val scheduling_startDate="startDate"
+            val scheduling_endDate="endDate"
+            val scheduling_startTime="startTime"
+            val scheduling_endTime="endTime"
+            val scheduling_value="Scheduling"
+
 
         }
 
 
     override fun onCreate(db: SQLiteDatabase) {
         val devices_query="CREATE TABLE $tableName($srNo_col Integer PRIMARY KEY,$device_id_col TEXT)"
+        val schedule_query="CREATE TABLE $tableSchedule($scheduling_Sr_No INTEGER PRIMARY KEY AUTOINCREMENT,$scheduling_everyDay TEXT,$scheduling_startDate TEXT,$scheduling_endDate TEXT,$scheduling_startTime TEXT,$scheduling_endTime TEXT,$scheduling_value TEXT)"
         db.execSQL(devices_query)
+        db.execSQL(schedule_query)
 
 
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL("DROP TABLE IF EXISTS $tableName")
+        db.execSQL("DROP TABLE IF EXISTS $tableSchedule")
         onCreate(db)
+
+    }
+
+    fun ScheduleRegistration(sch_everyday:String,sDate:String,eDate:String,sTime:String,eTime:String,sch_value:String){
+        val db=this.writableDatabase
+        val contentValues=ContentValues().apply {
+            put(scheduling_everyDay,sch_everyday)
+            put(scheduling_startDate, sDate)
+            put(scheduling_endDate,eDate)
+            put(scheduling_startTime,sTime)
+            put(scheduling_endTime,eTime)
+            put(scheduling_value,sch_value)
+
+
+        }
+
+        db.insert(tableSchedule,null,contentValues)
+        db.close()
 
     }
 
     fun RegisterUserHelpher(srNo:Int,device_id:String){
         val db=this.writableDatabase
-
         val cursor=db.rawQuery("SELECT * FROM $tableName WHERE $srNo_col=?" , arrayOf(srNo.toString()))
-
         val contentValues=ContentValues().apply {
             put(srNo_col,srNo)
             put(device_id_col,device_id,)
         }
-
-         if(cursor.count==0){
+        if(cursor.count==0){
              db.insert(tableName,null,contentValues)
-
-         }else{
+        }else{
              db.update(tableName,contentValues, "Sr_No=?", arrayOf(srNo.toString()))
          }
-
-
         db.close()
 
     }
---
+++ app/src/main/java/com/example/etcdynamiclight/ScheduleModel.kt
@@ -0,0 +1,11 @@
+package com.example.etcdynamiclight
+
+class ScheduleModel {
+
+    lateinit var sch_everyDay:String
+    lateinit var sch_sDate:String
+    lateinit var sch_eDate:String
+    lateinit var sch_sTime:String
+    lateinit var sch_eTime:String
+    lateinit var sch_value:String
+}
